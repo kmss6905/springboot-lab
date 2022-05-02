032bfe341251bbab64bf73c5018cd3007775c85d
kmss6905 <kmss69052@naver.com>
2022-05-02 22:57:57 +0900

    init testcode project

---
+++ spring-testcode/src/main/java/com/example/springtestcode/SpringTestcodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtestcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestcodeApplication.class, args);
+    }
+
+}
---
+++ spring-testcode/src/test/java/com/example/springtestcode/SpringTestcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtestcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
