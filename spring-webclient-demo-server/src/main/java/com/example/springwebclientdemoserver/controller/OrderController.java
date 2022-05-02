12ad15bb544db1388d20b33462afe42da5b2a2cb
kmss6905 <kmss69052@naver.com>
2022-05-02 21:09:55 +0900

    webclient demo server 컨트롤러 로깅 추가

--- spring-webclient-demo-server/src/main/java/com/example/springwebclientdemoserver/controller/OrderController.java
+++ spring-webclient-demo-server/src/main/java/com/example/springwebclientdemoserver/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.example.springwebclientdemoserver.controller;
 import com.example.springwebclientdemoserver.dto.OrderDto;
 import com.example.springwebclientdemoserver.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class OrderController {
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
 
     private final List<OrderDto> preOrders;
     private final OrderRepository orderRepository;
@@ -23,6 +26,7 @@ public class OrderController {
 
     @GetMapping("/v1/orders/{orderId}")
     public OrderDto getOrder(@PathVariable int orderId){
+        log.debug("order Id : {}", orderId);
         return orderRepository.getOrderByOrderId(orderId)
                 .orElseThrow( () -> new RuntimeException("not fount order"));
     }
