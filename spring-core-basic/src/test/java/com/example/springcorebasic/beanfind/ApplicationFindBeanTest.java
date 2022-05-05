8858224b9ded009f1ec5b17e00119c8a62eb65f7
kmss6905 <kmss69052@naver.com>
2022-05-05 20:07:44 +0900

    spring-core-basic, 스프링 container 빈 조회

---
+++ spring-core-basic/src/test/java/com/example/springcorebasic/beanfind/ApplicationFindBeanTest.java
@@ -0,0 +1,40 @@
+package com.example.springcorebasic.beanfind;
+
+import com.example.springcorebasic.config.AppConfig;
+import com.example.springcorebasic.order.OrderService;
+import com.example.springcorebasic.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationFindBeanTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("이름으로 빈 찾기")
+    void findBeanByName(){
+        Object bean = ac.getBean("orderService");
+        assertThat(bean).isInstanceOf(OrderServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름과 타입으로 빈 찾기")
+    void findBeanByNameAndType(){
+        OrderService bean = ac.getBean("orderService", OrderService.class);
+        assertThat(bean).isInstanceOf(OrderServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈을 찾지 못함")
+    void findBeanX(){
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxx"));
+    }
+
+}
