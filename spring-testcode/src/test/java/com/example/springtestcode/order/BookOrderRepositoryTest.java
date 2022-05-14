7b0d69a9e7b6f078e31d1ce30f76c1dd1effbb4f
kmss6905 <kmss69052@naver.com>
2022-05-14 12:32:58 +0900

    spring-testcode: add dao testcode

--- spring-testcode/src/test/java/com/example/springtestcode/order/BookOrderRepositoryTest.java
+++ spring-testcode/src/test/java/com/example/springtestcode/order/BookOrderRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.example.springtestcode.order;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +10,7 @@ import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -29,24 +32,28 @@ public class BookOrderRepositoryTest {
         System.out.println("mariaDBContainer.getJdbcUrl() : " + mariaDBContainer.getJdbcUrl());
     }
 
-//    @AfterAll
-//    static void afterAll() {
-////        mariaDBContainer.stop();
-//    }
-
-    @Test
-    void openTestcontainer() {
-        System.out.println("hello world");
+    @AfterAll
+    static void afterAll() {
+        mariaDBContainer.stop();
     }
 
-
     @Test
+    @DisplayName("주문 번호 id(Long) 로 주문 찾기 - 성공")
     void findByOrderId() {
         BookOrder save = orderRepository.save(new BookOrder("hello", 123L));
         BookOrder bookOrder = findOrder(save.getId());
         assertThat(bookOrder.getId()).isEqualTo(save.getId());
     }
-//
+
+    @Test
+    @DisplayName("주문 번호 id(Long) 로 주문 찾기 - 실패, 존재하지 않는 주문번호")
+    void finNonExistentBookOrderByOrderId(){
+
+        assertThatThrownBy(() -> findOrder(9999L), "존재하지 않는 주문 번호 검색 시 예외")
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("not found order");
+    }
+
     public BookOrder findOrder(Long orderId) {
         return orderRepository.findById(orderId)
                 .orElseThrow(() -> new RuntimeException("not found order"));
