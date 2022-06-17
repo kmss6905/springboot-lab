b3b7cd1dcf0e3e9efdb5d9746bf10a57f44f09e1
kmss6905 <kmss69052@naver.com>
2022-06-17 20:55:46 +0900

    start real web mvc project

---
+++ spring-mvc-first/src/main/java/com/example/springmvcfirst/SpringMvcFirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcFirstApplication.class, args);
+    }
+
+}
---
+++ spring-mvc-first/src/test/java/com/example/springmvcfirst/SpringMvcFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvcfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
