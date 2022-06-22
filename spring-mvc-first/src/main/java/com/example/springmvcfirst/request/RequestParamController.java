1e0c5e2cc08eac8ced5ea94d9e4b0ca7b380c831
kmss6905 <kmss69052@naver.com>
2022-06-22 23:46:53 +0900

    @ModelAttribute 추가

--- spring-mvc-first/src/main/java/com/example/springmvcfirst/request/RequestParamController.java
+++ spring-mvc-first/src/main/java/com/example/springmvcfirst/request/RequestParamController.java
@@ -1,20 +1,17 @@
 package com.example.springmvcfirst.request;
 
+import com.example.springmvcfirst.HelloData;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
+@Controller
 public class RequestParamController {
 
     @ResponseBody
@@ -26,6 +23,12 @@ public class RequestParamController {
         response.getWriter().write("ok");
     }
 
+    /**
+     * @RequestParam 사용
+     * - 파라미터 이름으로 바인딩
+     * @ResponseBody 추가
+     * - View 조회를 무시하고, HTTP message body에 직접 해당 내용 입력
+     */
     @ResponseBody
     @RequestMapping("/request-param-v2")
     public String requestParamV2(
@@ -89,10 +92,29 @@ public class RequestParamController {
         return "ok";
     }
 
+    /**
+     * @RequestParam Map, MultiValueMap
+     * Map(key=value)
+     * MultiValueMap(key=[value1, value2, ...]) ex) (key=userIds, value=[id1, id2])
+     */
     @ResponseBody
     @RequestMapping("/request-param-map")
     public String requestParamMap(@RequestParam MultiValueMap<String, Object> map) {
         log.debug("username={}, age={}", map.get("username"), map.get("age"));
+        return "ok";
+    }
+
+    /**
+     * @ModelAttribute 사용
+     * 참고: model.addAttribute(helloData) 코드도 함께 자동 적용됨, 뒤에 model을 설명할 때자세히 설명
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+//        HelloData helloData = new HelloData();
+//        helloData.setAge(age);
+//        helloData.setUsername(username);
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
         return "ok";
     }
 }
