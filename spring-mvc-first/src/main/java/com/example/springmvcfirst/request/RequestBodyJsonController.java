48f3f45712c3e9f6c4501a6748fae47ae39e54ea
kmss6905 <kmss69052@naver.com>
2022-06-23 23:47:00 +0900

    http message converter

--- spring-mvc-first/src/main/java/com/example/springmvcfirst/request/RequestBodyJsonController.java
+++ spring-mvc-first/src/main/java/com/example/springmvcfirst/request/RequestBodyJsonController.java
@@ -4,10 +4,8 @@ import com.example.springmvcfirst.HelloData;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -71,5 +69,22 @@ public class RequestBodyJsonController {
     public String requestBodyJsonV4(HttpEntity<HelloData> data) {
         log.info("username={}, age={}", data.getBody().getUsername(), data.getBody().getAge());
         return "ok";
+    }
+
+    /**
+     * @RequestBody 생략 불가능(@ModelAttribute 가 적용되어 버림)
+     * HttpMessageConverter 사용 -> MappingJackson2HttpMessageConverter (contenttype:
+    application/json)
+     *
+     * @ResponseBody 적용
+     * - 메시지 바디 정보 직접 반환(view 조회X)
+     * - HttpMessageConverter 사용 -> MappingJackson2HttpMessageConverter 적용
+    (Accept: application/json)
+     */
+    @ResponseBody
+    @PostMapping("/request-body-json-v5")
+    public HelloData requestBodyJsonV5(@RequestBody HelloData data) {
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return data;
     }
 }
